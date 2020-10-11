84e70593e763748ab2ba4c137addc6d745797741
chethanjs97 <chethanjschethu@gmail.com>
2020-10-11 13:44:36 +0530

    Added : Singlet Combination and Calculating Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-random=$((RANDOM%2))
+echo -e "Enter The Times to Toss Coin \c"
+read num
+Head=0
+Tail=0
 
-if [ $random -eq 1 ]
-then
-	 echo "Head"
-else
-	echo "Tail"
-fi
+for ((i=0; i<$num; i++))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		 ((Head++))
+	else
+		 ((Tail++))
+	fi
+done
+
+declare -A Coins
+Coins[((Head))]=$Head
+Coins[((Tail))]=$Tail
+echo ${Coins[@]}
+
+h=$(($Head*100))
+headper=$(($h/$num))
+t=$(($Tail*100))
+tailper=$(($t/$num))
+echo "head % : $headper , tails % : $tailper "
